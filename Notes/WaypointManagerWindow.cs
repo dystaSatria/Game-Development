67ad4afa014dabf34ea9f1e7489870cc928975b7
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2024-01-05 12:06:24 +0300

    Create WaypointManagerWindow.cs

---
+++ Notes/WaypointManagerWindow.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class WaypointManagerWindow : EditorWindow
+{
+    [MenuItem("Waypoint/Waypoints Editor Tools")]
+    public static void ShowWindow()
+    {
+        GetWindow<WaypointManagerWindow>("Waypoints Editor Tools");
+    }
+
+    public Transform waypointOrigin;
+
+    private void OnGUI()
+    {
+        SerializeObject obj = new SerializeObject(this);
+
+        EditorGUILayout.PropertyField(obj.FindProperty("waypointOrigin"));
+
+        if(waypointOrigin == null)
+        {
+            EditorGUILayout.HelpBox("Please assign a Point origin transform", MessageType.Warning);
+        }
+
+        else
+        {
+            EditorGUILayout.BeginVertical("box");
+            CreateButtons();
+            EditorGUILayout.EndVertical();
+        }
+
+        obj.ApplyModifiedProperties();
+
+    }
+
+    void CreateButtons()
+    {
+        if(GUILayout.Button("Create Waypoint"))
+        {
+            CreateWaypoint();
+        }
+    }
+
+    void CreateWaypoint()
+    {
+        GameObject waypointObject = new GameObject("WayPoint" + waypointOrigin.childCount, typeof(Waypoint));
+        waypointOrigin.transform.SetParent(waypointOrigin, false);
+
+        Waypoint waypoint = waypointObject.GetComponent<Waypoint>();
+
+
+        if (waypointOrigin.childCount > 1)
+        {
+            waypoint.previousWaypoint = waypointOrigin.GetChild(waypointOrigin.childCount - 2).GetComponent<Waypoint>();
+            waypoint.previousWaypoint.nextWaypoint = waypoint;
+
+            waypoint.transform.position = waypoint.previousWaypoint.transform.position;
+            waypoint.transform.forward = waypoint.previousWaypoint.transform.forward;
+
+        }
+
+        Selection.activeGameObject = waypoint.gameObject;
+
+    }
+}
