75afce500a04f2940f6515454b8f6241e5dd7f7e
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2024-01-17 21:50:06 +0300

    Add files via upload

---
+++ Trakya Drift/CAR RACING 3D GAME/PlayerCarController.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCarController : MonoBehaviour
+{
+    [Header("Wheels collider")]
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider backLeftWheelCollider;
+    public WheelCollider backRightWheelCollider;
+
+    [Header("Wheels Transform")]
+    public Transform frontLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+    public Transform backLeftWheelTransform;
+    public Transform backRightWheelTransform;
+
+    [Header("Car Engine")]
+    public float accelerationForce = 300f;
+    public float breakingForce = 3000f;
+    private float presentBreakForce = 0f;
+    private float presentAcceleration = 0f;
+
+    [Header("Car Steering")]
+    public float wheelsTorque = 35f;
+    private float presentTurnAngle = 0f;
+
+    private void Update()
+    {
+        MoveCar();
+        CarSteering();
+        ApplyBreaks();
+    }
+
+    private void MoveCar()
+    {
+        //Araba Gitmesi -> İleri Geri
+        frontLeftWheelCollider.motorTorque = presentAcceleration;
+        frontRightWheelCollider.motorTorque = presentAcceleration;
+        backLeftWheelCollider.motorTorque = presentAcceleration;
+        backRightWheelCollider.motorTorque = presentAcceleration;
+
+        presentAcceleration = accelerationForce * Input.GetAxis("Vertical");
+
+    }
+
+    private void CarSteering()
+    {
+        //Spor Araba Fizik (Drift de yapabilir)
+        presentTurnAngle = wheelsTorque * Input.GetAxis("Horizontal");
+        frontLeftWheelCollider.steerAngle = presentTurnAngle;
+        frontRightWheelCollider.steerAngle = presentTurnAngle;
+        SteeringWheels(frontLeftWheelCollider, frontLeftWheelTransform);
+        SteeringWheels(frontRightWheelCollider, frontRightWheelTransform);
+        SteeringWheels(backLeftWheelCollider, backLeftWheelTransform);
+        SteeringWheels(backRightWheelCollider, backRightWheelTransform);
+
+    }
+
+
+    private void SteeringWheels(WheelCollider WC, Transform WT)
+    {
+        Vector3 position;
+        Quaternion rotation;
+
+        WC.GetWorldPose(out position, out rotation);
+
+        WT.position = position;
+        WT.rotation = rotation;
+    }
+
+    public void ApplyBreaks()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            presentBreakForce = breakingForce;
+
+        else
+            presentBreakForce = 0f;
+
+
+        frontLeftWheelCollider.brakeTorque = presentBreakForce;
+        frontRightWheelCollider.brakeTorque = presentBreakForce;
+        backLeftWheelCollider.brakeTorque = presentBreakForce;
+        backRightWheelCollider.brakeTorque = presentBreakForce;
+
+    }
+
+
+}
